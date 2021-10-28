a9756dc6710b7453c04cf5e65d79f8a505df5260
Kenzi Stewart <kstewart@dployr.com>
2021-10-28 12:54:21 -0700

    added action

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM amazon/aws-cli
+
+COPY entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT [ "/entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# INPUTS
+
+# INPUT_AWS_ACCESS_KEY_ID
+AWS_ACCESS_KEY_ID=${INPUT_AWS_ACCESS_KEY_ID}
+# INPUT_AWS_SECRET_ACCESS_KEY
+AWS_SECRET_ACCESS_KEY=${INPUT_AWS_SECRET_ACCESS_KEY}
+# INPUT_AWS_REGION
+AWS_REGION=${INPUT_AWS_REGION}
+# INPUT_COMMAND
+# INPUT_SERVICE
+# INPUT_CLUSTER
+
+# fetch the most recent task
+ECS_TASK=$(${AWS_BIN} --profile="${AWS_PROFILE}" ecs list-tasks --cluster "${INPUT_CLUSTER}" --service="${INPUT_SERVICE}" | jq -r '.taskArns[]' | head -n 1)
+
+RESULT=$(aws ecs execute-command \
+  --cluster ${INPUT_CLUSTER} \
+  --non-interactive \
+  --task ${ECS_TASK} \
+  --command "${INPUT_COMMAND}")
+
+echo "::set-output name=result::${RESULT}"
